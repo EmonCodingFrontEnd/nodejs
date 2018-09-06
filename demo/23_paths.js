11b9df08cbd303817e20910579094495141fdf0d
Rushing <liming20110711@163.com>
2018-09-06 08:31:46 +0800

    update:20180906-02

--- demo/23_paths.js
+++ demo/23_paths.js
@@ -4,5 +4,6 @@ const mod = require('./02_cusmod');
 console.log(mod.testVar);
 
 console.log('__dirname    ', __dirname);
+console.log('__filename    ', __filename);
 console.log('process.cwd  ', process.cwd());
 console.log('./           ', path.resolve('./'));
