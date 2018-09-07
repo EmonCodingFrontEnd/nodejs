0df868ce173fd33378fd7ad59f2cdc227a317e43
Rushing <mingli@ishanshan.com>
2018-09-07 12:16:32 +0800

    update:20180907-04

--- 23_paths.js
+++
@@ -1,9 +0,0 @@
-const path = require('path');
-
-const mod = require('./02_cusmod');
-console.log(mod.testVar);
-
-console.log('__dirname    ', __dirname);
-console.log('__filename    ', __filename);
-console.log('process.cwd  ', process.cwd());
-console.log('./           ', path.resolve('./'));
