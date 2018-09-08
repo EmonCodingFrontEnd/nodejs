f2e9d41e0134ab2aab2578bf3ced742318df8b3d
Rushing <liming20110711@163.com>
2018-09-09 00:10:00 +0800

    update:20180908-03

--- src/helper/route.js
+++ src/helper/route.js
@@ -8,22 +8,35 @@ const config = require('../config/defaultConfig');
 const tplPath = path.join(__dirname, '../template/dir.html');
 const source = fs.readFileSync(tplPath);
 const template = Handlebars.compile(source.toString());
+const mime = require('./mime');
+const compress = require('./compress');
 
 module.exports = async function (req, res, filePath) {
   try {
     const stats = await stat(filePath);
     if (stats.isFile()) {
+      const contentType = mime(filePath);
       res.statusCode = 200;
-      res.setHeader('Content-Type', 'text/plain');
-      fs.createReadStream(filePath).pipe(res);
+      res.setHeader('Content-Type', contentType);
+      let rs = fs.createReadStream(filePath);
+      if (filePath.match(config.compress)) {
+        rs = compress(rs, req, res);
+      }
+      rs.pipe(res);
     } else if (stats.isDirectory()) {
       const files = await readdir(filePath);
       res.statusCode = 200;
       res.setHeader('Content-Type', 'text/html');
+      const dir = path.relative(config.root, filePath);
       const data = {
         title: path.basename(filePath),
-        dir: path.relative(config.root, filePath),
-        files: files
+        dir: dir ? `/${dir}` : '',
+        files: files.map(file => {
+          return {
+            file,
+            icon: mime(file)
+          };
+        })
       };
       res.end(template(data));
     }
