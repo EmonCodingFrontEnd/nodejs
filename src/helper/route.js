5aa8bcdce83dd4c85c5f070b12c00ff607fb7f35
Rushing <liming20110711@163.com>
2018-09-09 13:40:48 +0800

    update:20180909-01

--- src/helper/route.js
+++ src/helper/route.js
@@ -10,6 +10,7 @@ const source = fs.readFileSync(tplPath);
 const template = Handlebars.compile(source.toString());
 const mime = require('./mime');
 const compress = require('./compress');
+const range = require('./range');
 
 module.exports = async function (req, res, filePath) {
   try {
@@ -18,7 +19,15 @@ module.exports = async function (req, res, filePath) {
       const contentType = mime(filePath);
       res.statusCode = 200;
       res.setHeader('Content-Type', contentType);
-      let rs = fs.createReadStream(filePath);
+      let rs;
+      const {code, start, end} = range(stats.size, req, res);
+      if (code === 200) {
+        res.statusCode = 200;
+        rs = fs.createReadStream(filePath);
+      } else {
+        res.statusCode = 206;
+        rs = fs.createReadStream(filePath, {start, end});
+      }
       if (filePath.match(config.compress)) {
         rs = compress(rs, req, res);
       }
